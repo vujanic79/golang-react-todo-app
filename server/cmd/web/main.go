f05ee48a5944d5fedb20f373bb50b644cd94e723
milanvujanic <milanvujanic79@gmial.com>
2025-04-11 23:57:21 +0200

    refactor(docker,logging): Made Dockerfile build steps more efficient, introduced zerolog logger

--- server/cmd/web/main.go
+++ server/cmd/web/main.go
@@ -9,23 +9,23 @@ import (
 	"github.com/vujanic79/golang-react-todo-app/pkg/db/data"
 	"github.com/vujanic79/golang-react-todo-app/pkg/http_rest"
 	"github.com/vujanic79/golang-react-todo-app/pkg/logger"
-	"log"
 	"net/http"
 	"os"
 )
 
+const taskStatusesData = "./pkg/db/data/task_statuses.csv"
+
 func main() {
 	l := logger.Get()
-	portString := os.Getenv("PORT")
-	if portString == "" {
+	p := os.Getenv("PORT")
+	if p == "" {
 		err := errors.New("PORT environment variable not set")
-		l.Error().Stack().Err(errors.WithStack(err)).
+		l.Fatal().Stack().Err(errors.WithStack(err)).
 			Msg("Setting PORT environment variable error")
-		os.Exit(1)
 	}
 
-	router := chi.NewRouter()
-	router.Use(cors.Handler(cors.Options{
+	r := chi.NewRouter()
+	r.Use(cors.Handler(cors.Options{
 		AllowedOrigins:   []string{"http://localhost:3000"},
 		AllowedMethods:   []string{"GET", "POST", "PUT", "DELETE"},
 		AllowedHeaders:   []string{"*"},
@@ -35,7 +35,7 @@ func main() {
 	}))
 
 	dbQueries := db.GetPostgreSQLConnection()
-	data.LoadDataToDatabase(dbQueries, "./pkg/db/data/task_statuses.csv")
+	data.LoadDataToDatabase(dbQueries, taskStatusesData)
 
 	userRepository := db.NewUserRepository(dbQueries)
 	taskRepository := db.NewTaskRepository(dbQueries)
@@ -49,25 +49,26 @@ func main() {
 	taskController := http_rest.NewTaskController(taskService, userService)
 	taskStatusController := http_rest.NewTaskStatusController(taskStatusService)
 
-	router1 := chi.NewRouter()
-	router1.Post("/users", userController.CreateUser)
-	router1.Post("/tasks-by-user", taskController.GetTasksByUserId)
-	router1.Post("/tasks", taskController.CreateTask)
-	router1.Delete("/tasks/{taskId}", taskController.DeleteTask)
-	router1.Put("/tasks/{taskId}", taskController.UpdateTask)
-	router1.Post("/task-status", taskStatusController.CreateTaskStatus)
-	router1.Get("/task-status", taskStatusController.GetTaskStatuses)
-	router1.Get("/task-status/{taskStatus}", taskStatusController.GetTaskStatusByStatus)
+	subR := chi.NewRouter()
+	subR.Post("/users", userController.CreateUser)
+	subR.Post("/tasks-by-user", taskController.GetTasksByUserId)
+	subR.Post("/tasks", taskController.CreateTask)
+	subR.Delete("/tasks/{taskId}", taskController.DeleteTask)
+	subR.Put("/tasks/{taskId}", taskController.UpdateTask)
+	subR.Post("/task-status", taskStatusController.CreateTaskStatus)
+	subR.Get("/task-status", taskStatusController.GetTaskStatuses)
+	subR.Get("/task-status/{taskStatus}", taskStatusController.GetTaskStatusByStatus)
 
-	router.Mount("/todo", router1)
+	r.Mount("/todo", subR)
 
 	srv := &http.Server{
-		Handler: router,
-		Addr:    ":" + portString,
+		Handler: r,
+		Addr:    ":" + p,
 	}
 
 	err := srv.ListenAndServe()
 	if err != nil {
-		log.Fatal("Server failed to start", err.Error())
+		l.Fatal().Stack().Err(errors.WithStack(err)).
+			Msg("Starting HTTP server error")
 	}
 }
