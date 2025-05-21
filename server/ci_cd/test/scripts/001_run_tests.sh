9af8a7494f3bb5038f01fa072356bdda883552be
milanvujanic <milanvujanic79@gmial.com>
2025-05-21 14:13:14 +0200

    chore(ci_cd): Refactored jenkinsfile and shell script

--- server/ci_cd/test/scripts/001_run_tests.sh
+++ server/ci_cd/test/scripts/001_run_tests.sh
@@ -4,4 +4,6 @@ export PATH=$PATH:/usr/local/go/bin:$HOME/go/bin
 
 go install github.com/jstemmer/go-junit-report/v2@latest
 
-go test -v server/... | go-junit-report > report.xml
+cd server
+
+go test -v ./... | go-junit-report > report.xml
